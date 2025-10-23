0cab474c9d5c3419dff52a883b07d15bbac5069b
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-10-23 17:33:59 +0000

    adiciona novo exercício 1 com cálculo de conta de consumo

---
+++ aula-07-28.08/Exercicios/exercicio-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int assinatura_basica = 7;
+    float consumo;
+    float preco_11_30 = 1, preco_31_100 = 2, preco_101_mais = 5, vl_conta;
+
+    printf("Digite o consumo da residência:\n");
+    scanf("%f", &consumo);
+    if ((consumo > 100))
+    {
+        vl_conta = 7 + (20 * preco_11_30) + (70 * preco_31_100) + ((consumo - 100) * preco_101_mais);
+    }
+    else if (consumo > 30)
+    {
+        vl_conta = 7 + (20 * preco_11_30) + ((consumo - 30) * preco_31_100);
+    }
+    else if (consumo > 10)
+    {
+        vl_conta = 7 + ((consumo - 10) * preco_11_30);
+    }
+    else
+    {
+        vl_conta = 7;
+    }
+    printf("O valor da conta da sua conta é de R$ %.2f.", vl_conta);
+}
