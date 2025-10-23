2b3950d8f643cd84aa22a462ebfafe0638bc08fc
Gabriel Vieira <36107057+gb1456v@users.noreply.github.com>
2025-10-23 17:48:40 +0000

    refatora calculadora de peso ideal para melhorar a legibilidade e corrigir a lógica de entrada

--- aula-06-26.08/Exercicios/Exercicio-6.c
+++ aula-06-26.08/Exercicios/Exercicio-6.c
@@ -23,6 +23,21 @@ int main()
 =======
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    float altura,peso_ideal;
+    char sexo;
+
+    printf("---------------\n");
+    printf("Calculadora de peso ideal\n");
+    printf("---------------\n");
+    printf("Digite sua altura:\n");
+    scanf("%f",&altura);
+    printf("Digite seu sexo:(m=masculino ou F=feminino)");
+    scanf(" %c",&sexo);
+    if(sexo == 'm') {
+        peso_ideal = (72.7 * altura) - 58;
+    } else if (sexo == 'f') {
+        peso_ideal = (62.1 * altura) - 44.7;
+    }
+    printf("Seu peso ideal é %.2f", peso_ideal);
 }
