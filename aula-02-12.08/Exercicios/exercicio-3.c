3a283e71b111795aa8567395cce7bb7390622da2
Gabriel Vieira <36107057+gabriel-vieira14@users.noreply.github.com>
2026-03-12 11:39:41 +0000

    voltamos a estaca zero

--- aula-02-12.08/Exercicios/exercicio-3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a;
-    float b;
-    printf("Digite um número inteiro e um real:\n");
-    scanf("%d %f", &a, &b);
-    printf("O valor das variaveis a e b, respectivamente, sao: %d, %.2f \n", a, b);
-}
