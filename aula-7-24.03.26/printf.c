448c147cc2d03f4fd50b0c42179f3b709cdc6125
Gabriel Vieira <36107057+gabriel-vieira14@users.noreply.github.com>
2026-04-15 23:59:42 +0000

    código da aula 7

---
+++ aula-7-24.03.26/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    printf("%d\n", 40);
+    //imprime "40"
+    printf("O valor é %d.\n", 42);
+    //imprime "O valor é 42"
+    printf("%d+%d=%d", 10, a, 10+a);
+    //imprime "10+10=20"
+
+}
