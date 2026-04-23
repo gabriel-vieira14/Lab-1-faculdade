0c523295c8e00c6fde186ea2ea229e9f5522136c
gabriel-vieira14 <gabrielvieira794@gmail.com>
2026-04-23 06:47:44 -0300

    corrige definição de PI, ajusta tipo de retorno da função fatorial e adiciona função calcula_raiz

--- Trabalhos/Trabalho-1/parte-1.c
+++ Trabalhos/Trabalho-1/parte-1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-const double PI = 3.1415;
+#define PI 3.1415
 double pot(double x,int numero) {
     double res= 1.0;
     int numero_absoluto = numero;
@@ -28,7 +28,7 @@ void testa_pot() {
     resultado = pot(base, pot_max);
     printf("%.2lf elevado a %d é: %.5lf\n", base, pot_max, resultado);
 }
-long double fatorial(long int n) {
+double fatorial(int n) {
     long int mult=0;
     long double fat = 1;
     mult = n;
@@ -52,7 +52,36 @@ double conversao_graus_rad(double graus) {
     radianos = (graus * PI)/180;
     return radianos;
 }
-
+double calcula_raiz(double x) {
+    double chute,novo_chute, chute_abs, diferenca, diferenca_abs, raiz;
+    int loop=1;
+    printf("Chute um valor:\n");
+    scanf("%lf", &chute);
+    if(chute <= 0) {
+        printf("Você chutou um número inválido! Chute apenas valores positivos diferentes de 0!\n");
+        scanf("%lf", &chute);    
+    }
+    while(loop == 1)  {
+        // aqui calcula a média 
+        novo_chute = ((chute) + (x/chute)) / 2.0;
+        // diferença entre dois chutes sucessivos
+        diferenca = novo_chute - chute;
+        //valor absoluto da diferenca
+        if(diferenca < 0) {
+            diferenca_abs = -diferenca;
+        } else if(diferenca >= 0) {
+            diferenca_abs = diferenca;
+        }
+        //condição de parada
+        if(diferenca_abs < 0.0000000001) {
+            loop = 0;
+        }
+        // pra a variável chute não rodar sempre com os mesmos números
+        chute = novo_chute;
+    }
+    raiz = novo_chute; 
+    return raiz;
+}
 int main() {
     testa_pot();
     testa_fatorial();
