c0a868251c02650fb9db424ff496df8b15ad4553
Vinicius <viniciusluccarighi@gmail.com>
2026-04-28 13:11:08 -0300

    Correção exercicio-1

---
+++ Exercicios/aula-15/correção-exercicio-1.c
@@ -0,0 +1,49 @@
+// Faça uma função que calcula a raiz da equação
+//   ax² + bx + c = 0
+// usando o método de Bhaskara
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool bhaskara(double a, double b, double c, 
+              double *r1, double *r2)
+{ 
+  double delta = pow(b, 2) - 4 * a * c;
+  if (delta < 0) return false;
+  *r1 = (-b + sqrt(delta)) / (2 * a);
+  *r2 = (-b - sqrt(delta)) / (2 * a);
+  return true;
+}
+
+int main()
+{
+  double a, b, c;
+  double raiz1, raiz2;
+  printf("Digite os valores dos coeficientes ");
+  printf("a, b e c da equação 'a x² + b x + c'\n");
+  scanf("%ld%ld%ld", &a, &b, &c);
+  if (bhaskara(a, b, c, &raiz1, &raiz2)) {
+    printf("As raizes são %f e %f\n", raiz1, raiz2);
+  } else {
+    printf("Equação sem raízes reais\n");
+  }
+}
+
+
+int *p;
+int x = 67;
+int a;
+
+p = &x;
+
+a = *p;
+*p = 34;
+
+
+
+
+
+
+
+
